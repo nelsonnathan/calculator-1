cd339846367055dda72a2e520f01f782c0f99f22
Nate Nelson <nathan.nelson99@gmail.com>
2021-07-12 14:19:13 -0600

    adds additional calculator functions

--- calc.js
+++ calc.js
@@ -31,7 +31,23 @@ reader.question("What would you like to calculate?", function(input){
 	}
 
 	if (mathSymbol === "root") {
-		console.log(Math.sqrt(num1))
+		console.log(Math.sqrt(num1));
+	}
+
+	if (mathSymbol === "**") {
+		console.log(num1 ** num2);
+	}
+
+	if (mathSymbol === "%") {
+		console.log(num1 % num2);
+	}
+
+	if (mathSymbol === "square") {
+		console.log(num1 ** 2);
+	}
+
+	if (mathSymbol === "cube") {
+		console.log(num1 ** 3);
 	}
 
 	// This line closes the connection to the command line interface.
