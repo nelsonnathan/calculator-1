0cdf5f9e5f477a74f333d30b7a095f97aea2717a
Nate Nelson <nathan.nelson99@gmail.com>
2021-07-12 13:51:50 -0600

    finished calculator

--- calc.js
+++ calc.js
@@ -6,7 +6,6 @@ const reader = readline.createInterface({
 });
 
 
-
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
@@ -15,7 +14,28 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 
+	if (mathSymbol === "+") {
+		console.log(num1 + num2);
+	}
+
+	if (mathSymbol === "-") {
+		console.log(num1 - num2);
+	}
+
+	if (mathSymbol === "*") {
+		console.log(num1 * num2);
+	}
+
+	if (mathSymbol === "/") {
+		console.log(num1 / num2);
+	}
+
+	if (mathSymbol === "root") {
+		console.log(Math.sqrt(num1))
+	}
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
